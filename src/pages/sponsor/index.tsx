f25d284430fa0207723c8b21290292d4f7c60f8e
lencx <cxin1314@gmail.com>
2022-11-22 13:46:02 +0800

    chore: sponsor

--- src/pages/sponsor/index.tsx
+++ src/pages/sponsor/index.tsx
@@ -9,8 +9,7 @@ export default function Sponsor() {
       <h2>Sponsor</h2>
       <p>🙏 创作不易，如果文章对您有所帮助，可以请我喝杯咖啡，感恩！</p>
       <div>
-        <img src={require('./img/wechat.png').default} alt="微信支付" />
-        <img src={require('./img/alipay.png').default} alt="支付宝支付" />
+        <img src={require('./img/sponsor.png').default} alt="赞赏码" />
       </div>
     </Layout>
   );
